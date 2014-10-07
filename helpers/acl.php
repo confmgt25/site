3fb9b66159e7982a2d4d1ce4c661a953b66abd42
Kaushal Keraminiyage <kkeraminiyage@yahoo.co.uk>
2014-10-07 14:33:12 +0100

    Reviewer authentication updated
    Reviewer authentication now includes optional $paperid parameter.

--- helpers/acl.php
+++ helpers/acl.php
@@ -77,7 +77,7 @@ abstract class AclHelper
 		return $result; 
 	}
 	
-	public static function isRev1ewer($themeid = 0)
+	public static function isRev1ewer($paperid = 0)
 	{
 		//Obtain a database connection
 		$db = JFactory::getDbo();
@@ -87,32 +87,62 @@ abstract class AclHelper
 			return false;
 		}
 		
-		if ($themeid == 0) {
+		if ($paperid == 0) {
 		
 			//Build the query 
 			$query = $db->getQuery(true)
-						->select('COUNT(*)')
-						->from($db->quoteName('#__confmgt_rev1ewers'))
-						->where('created_by = '. (int)$user->id);
+						->select($db->quoteName(array('a.id', 'a.userid')))
+						->from($db->quoteName('#__confmgt_rev1ewers', 'a'))
+						->where('a.userid = '. (int)$user->id);
 		}else{
 						//Build the query 
 			$query = $db->getQuery(true)
-						->select('COUNT(*)')
-						->from($db->quoteName('#__confmgt_rev1ewers'))
-						->where('theme = '. (int)$themeid); 
+						->select($db->quoteName(array('a.*', 'b.username', 'b.name', 'b.id')))
+						->from($db->quoteName('#__confmgt_rev1ewers_papers', 'a'))
+						->join('INNER', $db->quoteName('#__users', 'b') . ' ON (' . $db->quoteName('a.userid') . ' = ' . $db->quoteName('b.id').')')
+						->where('b.id = '. (int)$user->id)
+						->where('a.paperid = '. (int)$paperid); 
 		}
 			
 		//Prepare the query
 		$db->setQuery($query);
-		// Load the row.
-		$row = $db->loadResult();	
-		//check if user ceareted the record 
-		$result = ($row > 0);
-		//Return the Hello
+		$rows = $db->loadAssocList();
+		if (empty($rows)) {
+			$result = false;
+		}else{
+			$result = true;
+		}
+		
 		return $result; 
 	}
 	
+	// function to get userid by entering the email 
 	
+	public static function getUserID($email)
+	{
+		$db = JFactory::getDbo();
+		$user = JFactory::getUser();
+		
+		if ($user->id == 0) {
+			return false;
+		}
+			$query = $db->getQuery(true)
+						->select('id, email')
+						->from($db->quoteName('#__confmgt_users'))
+						->where('email = '. $email);
+			
+		//Prepare the query
+		$db->setQuery($query); 
+		// Load the row.
+		$row = $db->loadObject();
+		
+		if ($row->id > 0) {
+			return $row->id;
+		}else{
+			return false;
+		}
+	}
+		
 	
 }
 
